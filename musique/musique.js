8369089f9f4cda004cb00cfa4b7a51c381de838f
Léo Chely <31514354+leochely@users.noreply.github.com>
2018-10-15 23:37:10 -0600

    Links added

--- musique/musique.js
+++ musique/musique.js
@@ -1,7 +1,34 @@
 function showVideo(i){
 	$('#video, #overlay').fadeIn('slow');
-    $('#video').html('<iframe width="560" height="315" src="https://www.youtube.com/embed/coyDLR8OxmA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
-	
+	switch(i){
+		case 1:
+			$('#video').html('<iframe width="560" height="315" src="https://www.youtube.com/embed/gncLO8hr0gw" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
+			break;
+		case 2:
+			$('#video').html('<iframe width="560" height="315" src="https://www.youtube.com/embed/kZ2tVyNoj6E" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
+			break;
+		case 3:
+			$('#video').html('<iframe width="560" height="315" src="https://www.youtube.com/embed/coyDLR8OxmA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
+			break;
+		case 4:
+			$('#video').html('<iframe width="560" height="315" src="https://www.youtube.com/embed/GwSdT0lZOV4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
+			break;
+		case 5:
+			$('#video').html('<iframe width="560" height="315" src="https://www.youtube.com/embed/uXPvrwHjVZk" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
+			break;
+		case 6:
+			$('#video').html('<iframe width="560" height="315" src="https://www.youtube.com/embed/MfydP0-onAo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
+			break;
+		case 7:
+			$('#video').html('<iframe width="560" height="315" src="https://www.youtube.com/embed/SfyNekcNRg4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
+			break;
+		case 8:
+			$('#video').html('<iframe width="560" height="315" src="https://www.youtube.com/embed/tnPSoe9ZaeY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
+			break;
+		default:
+			$('#video').html("Cette musique n'est pas encore disponible sur Youtube"); 
+			break;
+	}
 }
 
 $(document).mouseup(function (e)
@@ -12,3 +39,4 @@ $(document).mouseup(function (e)
         $('#video').html('');
     }
 });
+
