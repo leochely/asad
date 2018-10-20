c6147cf30790336f4b33dbbc3ebe9f91cb6585c6
Léo Chely <31514354+leochely@users.noreply.github.com>
2018-10-19 21:17:24 -0500

    Footer at proper position now

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function hideLogo(){
 	$("#logo_container").fadeOut();
-	$("#main_content").delay(300).fadeIn();
-	$("footer").delay(300).fadeIn();
+	$("#wrapper").delay(300).fadeIn();
 }
 
 $(document).ready(function(){
